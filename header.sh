d5447354f3515e1ce3e94fc604ba89d0b2d5ba73
Oviawe007 <josephoviawe4@gmail.com>
2024-04-30 17:34:48 +0100

    header and variable script added

---
+++ header.sh
@@ -0,0 +1,7 @@
+#!/bin/bas
+#
+# Author: Efosa Oviawe
+# Date : 30/04/2024
+# Comment: This is the structure of a typical bash script. Always use comment to describe actions.
+#
+######################################## 
