94ea1396523b12472fda1660ad7097d9b3cc6d59
Oviawe007 <josephoviawe4@gmail.com>
2024-05-22 17:34:10 +0100

    hostname change

--- greet.sh
+++ greet.sh
@@ -8,17 +8,18 @@
 date_func(){
 
 	hour=`date +%H`
+	read -p "What is your name : " name
 	if [ $hour -lt 12 ] # if hour is less than 12
 	then
-	echo "GOOD MORNING WORLD"
+	echo "GOOD MORNING, ${name^^}"
 	elif [ $hour -le 16 ] # if hour is less than equal to 16
 	then
-	echo "GOOD AFTERNOON WORLD"
+	echo "GOOD AFTERNOON, ${name^^}"
 	elif [ $hour -le 20 ] # if hour is less than equal to 20
 	then
-	echo "GOOD EVENING WORLD"
+	echo "GOOD EVENING, ${name^^}"
 	else
-	echo "GOOD NIGHT WORLD"
+	echo "GOOD NIGHT, ${name^^}"
 	fi
 }
 
